14a28e372d245e65695c00a23ca103389f36bde4
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-04-22 11:05:34 -0400

    set PORT as an enviroment variable

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 
 const server = express();
+
+const PORT  = process.env.PORT || 8080;
+
 server.get("/heartbeat", function (req, res) {
     res.json({
         "is": "working"
     })
 });
 
-server.listen(8080, () => console.log("listening on port 8080."));
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
